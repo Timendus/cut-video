2fc41e34a4a39a559d41b845cf5124bd0ae733cb
Tim Franssen <mail@timendus.com>
2025-07-28 16:28:42 +0200

    First version

---
+++ README.md
@@ -0,0 +1,68 @@
+# Video cutting script
+
+Do you need to cuts parts from a video on a regular basis? For example so you
+can upload individual presentations from a longer live-streamed meeting? Just
+tell this script what to cut out and set it loose.
+
+## Dependencies
+
+Here's the brew summary, adapt to your own package manager:
+
+```bash
+brew install python3 ffmpeg
+```
+
+## Installation
+
+```bash
+pip install git+https://github.com/timendus/cut-video
+```
+
+Or if you prefer pipx:
+
+```bash
+pipx install git+https://github.com/timendus/cut-video
+```
+
+## Usage
+
+Create a text file defining the cuts you want to make. The file should contain
+two columns, one with start and optional end times and one with descriptions of
+the parts. The columns are separated by whitespace:
+
+```
+# TIME(S)         DESCRIPTION
+# -------         -----------
+00:00             Welcome
+01:48-08:55       New colleagues
+08:51-10:52       Agenda
+```
+
+Empty lines and lines starting with a `#` will be ignored. If only a starting
+time is given, the end time will default to the start time of the next part (or
+the end of the video). Start time and end time should be noted in `hh:mm:ss` or
+`mm:ss` or `ss`. Parts may overlap.
+
+Then run `cut-video`, passing in the source video and the text file you just
+created:
+
+```bash
+cut-video ~/Videos/my-video.mp4 ~/Videos/my-video.cuts
+```
+
+This will create a directory `~/Videos/my-video/`, based on the file name of the
+source video, and put all the parts you specified in there. In this example:
+
+```
+~/Videos/my-video/01 Welcome.mp4
+~/Videos/my-video/02 New colleagues.mp4
+~/Videos/my-video/03 Agenda.mp3
+```
+
+If you run the command again, ffmpeg will ask for each file if you wish to
+overwrite it or skip it. If you want to overwrite all files, you can pass
+`--overwrite` as the third argument to `cut-video`:
+
+```bash
+cut-video ~/Videos/my-video.mp4 ~/Videos/my-video.cuts --overwrite
+```
---
+++ cut_video/cli.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import re
+import ffmpeg
+
+
+def parse_cuts_file(cuts_file):
+    cuts = []
+    with open(cuts_file, "r") as f:
+        for i, line in enumerate(f):
+            line = line.strip()
+
+            # Skip empty lines and comments
+            if not line or line.startswith("#"):
+                continue
+
+            times, *description = line.split()
+
+            if len(times.split("-")) == 2:
+                start, end = times.split("-")
+            else:
+                start = times
+                end = None
+
+            cuts.append(
+                {
+                    "index": len(cuts) + 1,
+                    "start": start,
+                    "end": end,
+                    "description": " ".join(description),
+                }
+            )
+
+    # Fill in missing end times and calculate seconds
+    for i, cut in enumerate(cuts):
+        if cut["end"] == None and i < len(cuts) - 1:
+            cut["end"] = cuts[i + 1]["start"]
+
+        cut["start_seconds"] = parse_timestamp(cut["start"])
+        if cut["end"] is not None:
+            cut["end_seconds"] = parse_timestamp(cut["end"])
+
+    return cuts
+
+
+def parse_timestamp(timestamp):
+    parts = timestamp.split(":")
+    if len(parts) == 1:
+        return int(parts[0])
+    elif len(parts) == 2:
+        return int(parts[0]) * 60 + int(parts[1])
+    elif len(parts) == 3:
+        return int(parts[0]) * 3600 + int(parts[1]) * 60 + int(parts[2])
+    else:
+        raise ValueError("Invalid timestamp format")
+
+
+def output_filename(input_file, cut):
+    directory = os.path.splitext(input_file)[0]
+    extension = os.path.splitext(input_file)[1]
+    safe_description = re.sub(r"[^\w_. -]", "_", cut["description"])
+    return f"{directory}/{cut['index']:02d} {safe_description}{extension}"
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: cut-video <input_file> <cuts-file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    cuts = parse_cuts_file(sys.argv[2])
+    overwrite = len(sys.argv) > 3 and sys.argv[3].lower() == "--overwrite"
+
+    target_directory = os.path.splitext(input_file)[0]
+    if not os.path.exists(target_directory):
+        os.mkdir(target_directory)
+
+    for cut in cuts:
+        if cut["end"] is None:
+            cut["end_seconds"] = ffmpeg.probe(input_file)["format"]["duration"]
+
+        video = (
+            ffmpeg.input(input_file)
+            .trim(start=cut["start_seconds"], end=cut["end_seconds"])
+            .setpts("PTS-STARTPTS")
+        )
+
+        audio = (
+            ffmpeg.input(input_file)
+            .filter_("atrim", start=cut["start_seconds"], end=cut["end_seconds"])
+            .filter_("asetpts", "PTS-STARTPTS")
+        )
+
+        ffmpeg.output(video, audio, output_filename(input_file, cut)).run(
+            overwrite_output=overwrite
+        )
